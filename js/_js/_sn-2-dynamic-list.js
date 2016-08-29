bdbbb9b5d994c647fd11615092d285d71b0881b9
FelipePinhoUFC <felipe76857685@gmail.com>
2016-08-29 16:21:27 -0300

    Atualizando recurssão

--- js/_js/_sn-2-dynamic-list.js
+++ js/_js/_sn-2-dynamic-list.js
@@ -49,6 +49,8 @@ var DynamicList = function(rootEl, settings) {
 		// Trás os valores padrão para os elementos da lista
 		defaultData: {}, 
 
+		recurssiveIndex: 0, 
+
 		// Lista de callbacks
 		// Para que o item seja adicionado, removido ou editado, retorne true
 		onItemAdd: function(item) { return true; }, 
@@ -107,9 +109,22 @@ var DynamicList = function(rootEl, settings) {
 
 	this.doIncrementItem = function(name, index) {
 		
-		if( /[\w]+\[[\d]+\]\.[\w]+/.test(name) ) {
-			return name.replace( /\[[\d]+\]+/, "[" + index + "]")
-		} 
+		if(name) {
+			var reStr = "[\\w\\d]+\\[)(\\d+)(\\][\.\\w\\d\\[\\]]*)";
+
+			for(var i = 0; i < this.settings.recurssiveIndex; i++) {
+				reStr = "[\\w\\d\\[\\]]+\\." + reStr;
+			}
+			reStr = "(" + reStr;
+
+			regExp = new RegExp(reStr, 'g');
+
+			if( regExp.test(name) ) {
+				name = name.replace(regExp, "$1" + index + "$3")
+			} 
+
+			console.log(name + ":" + regExp + " : " + name);
+		}
 
 		return name;
 
@@ -117,6 +132,7 @@ var DynamicList = function(rootEl, settings) {
 
 	this.sortItems = function() {
 		var items = $(this.settings.cloneableElement);
+
 		items.sort(function (a, b) {
 			var contentA = parseInt( $(a).data('sort'));
 			var contentB = parseInt( $(b).data('sort'));
